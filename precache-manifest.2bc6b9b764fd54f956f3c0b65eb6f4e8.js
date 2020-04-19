0ad8fb417b2d2251e6294e2c8aa8c599c59361e5
Horea <horea.ginsca@gmail.com>
2020-04-19 11:20:30 +0300

    Updates

---
+++ precache-manifest.2bc6b9b764fd54f956f3c0b65eb6f4e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536a2a49bca76c3109b3a7f93f7ab7dc",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "acefefc4a07a17412a48",
+    "url": "/Covid-19-Tracker/static/css/main.b90014a4.chunk.css"
+  },
+  {
+    "revision": "21040fe66473df2704c7",
+    "url": "/Covid-19-Tracker/static/js/2.9f9cddaa.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.9f9cddaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acefefc4a07a17412a48",
+    "url": "/Covid-19-Tracker/static/js/main.a11e63a6.chunk.js"
+  },
+  {
+    "revision": "eebaca2ca868991201f4",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.9f3d5c86.js"
+  }
+]);
